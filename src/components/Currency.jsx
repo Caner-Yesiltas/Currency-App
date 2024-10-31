19fd106cc3ffc9e771c6d4bb99c63d4bf775280b
CanerYesiltas <caneryesiltas1@gmail.com>
2024-10-30 20:22:04 -0400

    add input states and form event handlers

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/currency.css"
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Currency = () => {
+
+    const [amount, setAmount] = useState(0);
+    const [fromCurrency, setFromCurrency] = useState("");
+    const [toCurrency, setToCurrency] = useState('');
+    const [result, setResult] =useState(0);
+
+
   return (
     <div className="currency-div" >
           <div className="conversion-row">
       
-      <input type="number" className='amount' />
-      <select className='from-currency-option' >
+      <input 
+      type="number" 
+      className='amount'
+      value={amount} onChange={(e) => setAmount(e.target.value)}  />
+
+      <select className='from-currency-option' 
+      value={fromCurrency}
+      onChange={(e)=> setFromCurrency(e.target.value)}
+
+      >
 
         <option>USD</option>
         <option>EUR</option>
@@ -18,7 +33,11 @@ const Currency = () => {
 
       <FaLongArrowAltRight style ={{ fontSize:'1.25rem', marginRight:".625rem"}} />
 
-      <select className='to-currency-option' >
+      <select className='to-currency-option' 
+      
+      value={toCurrency}
+      onChange={(e)=> setToCurrency(e.target.value)}
+      >
 
         <option>USD</option>
         <option>EUR</option>
@@ -30,7 +49,10 @@ const Currency = () => {
       </div>
 
        <div >
-       <button className='convert-button' > Convert </button>
+       <button className='convert-button'
+       
+      
+       > Convert </button>
        </div>
        
 
